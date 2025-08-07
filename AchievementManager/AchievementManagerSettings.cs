f766a3e6a90ddb386ed08cbd591cd2d3d9596054
Michael <sven.maier15@icloud.com>
2025-08-07 21:45:05 +0200

    Initial commit

---
+++ AchievementManager/AchievementManagerSettings.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Colossal;
+using Colossal.IO.AssetDatabase;
+using Colossal.PSI.Common;
+using Game.Modding;
+using Game.Settings;
+using Game.UI.Widgets;
+
+namespace AchievementManager
+{
+    [FileLocation("ModsSettings/AchievementManager/")]
+    public class AchievementManagerSettings : ModSetting
+    {
+        public AchievementManagerSettings(IMod mod) : base(mod)
+        {
+            SetDefaults();
+        }
+
+        [SettingsUIDropdown(typeof(AchievementManagerSettings), nameof(GetAchievementDropdownItems))]
+        public string SelectedAchievementKey { get; set; } = "First";
+
+        public IAchievement SelectedAchievement =>
+            PlatformManager.instance.EnumerateAchievements()
+                .FirstOrDefault(a => a.internalName == SelectedAchievementKey);
+
+        [SettingsUIButtonGroup("Achievement Actions")]
+        [SettingsUIButton]
+        public bool UnlockAchievement
+        {
+            set => PlatformManager.instance.UnlockAchievement(SelectedAchievement.id);
+        }
+
+        [SettingsUIButtonGroup("Achievement Actions")]
+        [SettingsUIButton]
+        public bool ResetAchievements
+        {
+            set => PlatformManager.instance.ResetAchievements();
+        }
+
+
+        public override void SetDefaults()
+        {
+            //dropdownItemsVersion = 0;
+        }
+
+        public DropdownItem<string>[] GetAchievementDropdownItems()
+        {
+            return PlatformManager.instance.EnumerateAchievements()
+                .Select(a => new DropdownItem<string>
+                {
+                    value = a.internalName,
+                    displayName = a.internalName // or use localization if needed
+                })
+                .ToArray();
+        }
+    }
+
+    public class LocaleEN : IDictionarySource
+    {
+        private readonly AchievementManagerSettings m_Setting;
+
+        public LocaleEN(AchievementManagerSettings setting)
+        {
+            m_Setting = setting;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors,
+            Dictionary<string, int> indexCounts)
+        {
+            return new Dictionary<string, string>
+            {
+                { m_Setting.GetSettingsLocaleID(), "AchievementManager" },
+                {
+                    m_Setting.GetOptionLabelLocaleID(nameof(AchievementManagerSettings.SelectedAchievementKey)),
+                    "Selected Dropdown"
+                },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(AchievementManagerSettings.SelectedAchievementKey)),
+                    "Select an achievement to unlock or reset."
+                },
+                {
+                    m_Setting.GetOptionLabelLocaleID(nameof(AchievementManagerSettings.UnlockAchievement)),
+                    "Unlock Achievement"
+                },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(AchievementManagerSettings.UnlockAchievement)),
+                    " Unlock the selected achievement."
+                },
+                {
+                    m_Setting.GetOptionLabelLocaleID(nameof(AchievementManagerSettings.ResetAchievements)),
+                    "Reset Achievements"
+                },
+                {
+                    m_Setting.GetOptionDescLocaleID(nameof(AchievementManagerSettings.ResetAchievements)),
+                    "Reset all achievements."
+                }
+            };
+        }
+
+        public void Unload()
+        {
+        }
+    }
+}
---
+++ AchievementManager/Mod.cs
@@ -0,0 +1,48 @@
+﻿using Colossal.Logging;
+using Colossal.Serialization.Entities;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace AchievementManager
+{
+    public class Mod : IMod
+    {
+        public static readonly ILog LOG = LogManager.GetLogger($"{nameof(AchievementManager)}.{nameof(Mod)}")
+            .SetShowsErrorsInUI(false);
+
+
+        public static Mod Instance { get; private set; }
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            Instance = this;
+            LOG.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                LOG.Info($"Current mod asset at {asset.path}");
+
+
+            updateSystem.World.GetOrCreateSystemManaged<ModExtensions>();
+        }
+
+        public void OnDispose()
+        {
+            LOG.Info(nameof(OnDispose));
+        }
+    }
+
+    public partial class ModExtensions : GameSystemBase
+    {
+        protected override void OnUpdate()
+        {
+        }
+
+        protected override void OnGameLoadingComplete(Purpose purpose, GameMode mode)
+        {
+            var myModSetting = new AchievementManagerSettings(Mod.Instance);
+            myModSetting.RegisterInOptionsUI();
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(myModSetting));
+        }
+    }
+}
